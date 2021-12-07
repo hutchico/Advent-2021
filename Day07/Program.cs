cf0dc069fa3c8ad648320b3b421f81cc74da348d
hutchico <22420744+hutchico@users.noreply.github.com>
2021-12-07 13:04:58 -0800

    day 7 commit

---
+++ Day07/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day07 {
+    class Program {
+        static void Main() {
+            List<int> crabs = new List<int>();
+            double fuel = 0;
+            int median;
+            int avg = 0;
+            using(StreamReader file = new StreamReader("input.txt")) {
+                string[] tkn = file.ReadLine().Split(',');
+                for(int i = 0; i < tkn.Length; i++)
+                    crabs.Add(Int32.Parse(tkn[i]));
+            }
+            crabs.Sort();
+            //theory: minimum distance is the median value of the array?
+            median = crabs[crabs.Count / 2];
+            for(int i = 0; i < crabs.Count; i++)
+                fuel += Math.Sqrt(Math.Pow(crabs[i] - median,2)); //account for negative values
+            Console.WriteLine("Fuel needed: " + fuel);
+
+            //part 2:
+            //theory: go from median value to average value?
+            for(int i = 0; i < crabs.Count; i++)
+                avg += crabs[i];
+            avg /= crabs.Count;
+            fuel = 0;
+            for(int i = 0; i < crabs.Count; i++)
+                fuel += compute_fuel(crabs[i],avg);
+            Console.WriteLine("Fuel needed: " + fuel);
+        }
+
+        static int compute_fuel(int start,int end) {
+            double distance = Math.Sqrt(Math.Pow(start - end,2));
+            int total = 0;
+            for(int i = 0; i < distance; i++) 
+                total += i + 1;
+            return total;
+        }
+    }
+}
