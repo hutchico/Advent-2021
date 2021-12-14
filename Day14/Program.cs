50dc70c5b7990dc34172075e674fb265c2cfd9e7
hutchico <22420744+hutchico@users.noreply.github.com>
2021-12-14 15:57:14 -0800

    code cleanup

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day14 {
     class Program {
@@ -10,21 +11,15 @@ namespace Day14 {
             Dictionary<string,Int64> count = new Dictionary<string,long>();
 
             using(StreamReader file = new StreamReader("input.txt")) {
-                char inp;
                 while(true) {
-                    inp = (char)file.Read();
-                    if(inp == '\n')
+                    char inp = (char)file.Read();
+                    if(inp == '\n') //read to end of first line
                         break;
-                    else if(inp == '\r') {
-                        file.Read();
-                        break;
-                    }
                     start.Add(inp);
                 }
-                string[] tk;
-                file.Read(); //ignore whitespace line and carriage return if applicable
+                file.Read(); //ignore whitespace line
                 while(!file.EndOfStream) {
-                    tk = file.ReadLine().Split(" -> ");
+                    string[] tk = file.ReadLine().Split(" -> ");
                     chemicals[tk[0]] = tk[1][0];
                 }
             }
@@ -32,10 +27,7 @@ namespace Day14 {
             for(int i = 1; i < start.Count; i++) {
                 string each = "";
                 each += start[i - 1].ToString() + start[i].ToString();
-                if(count.ContainsKey(each))
-                    count[each] += 1;
-                else
-                    count[each] = 1;
+                count[each] = count.ContainsKey(each) ? count[each] + 1 : 1;
             }
 
             //part 1: i < 10
@@ -43,8 +35,7 @@ namespace Day14 {
             for(int i = 0; i < 40; i++) {
                 count = smartStep(count,chemicals);
             }
-
-            Console.WriteLine("Difference between most common and least common after 40: " + smart_count(count,start[start.Count - 1]));
+            Console.WriteLine("Difference between most common and least common after 40 steps: " + smart_count(count,start[start.Count - 1]));
         }
 
         static Dictionary<string,Int64> smartStep(Dictionary<string,Int64> tracker,Dictionary<string,char> chemicals) {
@@ -52,27 +43,14 @@ namespace Day14 {
             //idea: insert C into NH -> increment NC, CH by 1
             foreach(KeyValuePair<string,Int64> entry in tracker) {
                 if(chemicals.ContainsKey(entry.Key)) {
-                    string part1;
-                    string part2;
+                    string part1, part2;
                     part1 = entry.Key.Insert(1,chemicals[entry.Key].ToString());
                     part2 = part1.Substring(1,2);
                     part1 = part1.Remove(2);
                     //we now have two strings, ie NC and CH
-                    if(temp.ContainsKey(part1)) {
-                        temp[part1] += entry.Value;
-                    }
-                    else {
-                        temp[part1] = entry.Value;
-                    }
-                    if(temp.ContainsKey(part2)) {
-                        temp[part2] += entry.Value;
-                    }
-                    else {
-                        temp[part2] = entry.Value;
-                    }
+                    temp[part1] = temp.ContainsKey(part1) ? temp[part1] + entry.Value : entry.Value;
+                    temp[part2] = temp.ContainsKey(part2) ? temp[part2] + entry.Value : entry.Value;
                     temp[entry.Key] -= entry.Value;
-                    if(temp[entry.Key] == 0)
-                        temp.Remove(entry.Key);
                 }
             }
             return temp;
@@ -82,18 +60,11 @@ namespace Day14 {
             Dictionary<char,Int64> specifics = new Dictionary<char,long>();
             foreach(KeyValuePair<string,Int64> entry in count) {
                 char c1;
-                c1 = entry.Key[0];
-                if(specifics.ContainsKey(c1))
-                    specifics[c1] += entry.Value;
-                else
-                    specifics[c1] = entry.Value;
+                c1 = entry.Key[0]; //count first letter of each pair
+                specifics[c1] = specifics.ContainsKey(c1) ? specifics[c1] + entry.Value : entry.Value;
             }
-
-            specifics[last] += 1;
-            List<Int64> dumpCounts = new List<long>(specifics.Values);
-            dumpCounts.Sort();
-
-            return dumpCounts[dumpCounts.Count - 1] - dumpCounts[0]; //don't even need to know specifically what it is lmao
+            specifics[last] += 1; //account for last character in the input string since it will never ever be modified
+            return specifics.Values.Max() - specifics.Values.Min(); //don't even need to know specifically what it is lmao
         }
     }
 }
