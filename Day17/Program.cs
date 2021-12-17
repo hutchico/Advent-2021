801ce8c077de3d4da61a333c85caf32303944e7e
hutchico <22420744+hutchico@users.noreply.github.com>
2021-12-17 14:59:22 -0800

    cleanup

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Day17 {
     class Program {
@@ -26,12 +26,12 @@ namespace Day17 {
             }
             //determine X range:
             max_x = xrange.Item2 + 1; //sanity check: x velocity > max range will overshoot target immediately
-            (int, int) x_window = find_x(min_x, max_x, xrange);
+            (int, int) x_window = find(min_x, max_x, xrange, 'x');
 
             //determine Y range:
             min_y = yrange.Item1 - 1; //sanity check: y velocity < max range will overshoot target immediately
             max_y = yrange.Item2 * -1 * 10; //educated guess based on original numbers, can adjust if necessary
-            (int, int) y_window = find_y(min_y, max_y, yrange);
+            (int, int) y_window = find(min_y, max_y, yrange, 'y');
 
             //we have a range of values for X and Y which will land it in the window in question
             //now we run each until it's outside that window and see what the max height is
@@ -46,30 +46,33 @@ namespace Day17 {
                     int ret = find_height(i, j);
                     if(ret > max_height)
                         max_height = ret;
-                    
                 }
             }
             Console.WriteLine("Max height: " + max_height);
             Console.WriteLine("Total valid coords: " + valid.Count);
         }
 
-        static (int,int) find_x(int minX, int maxX, (int, int) xrange) {
+        static (int, int) find(int min_val, int max_val, (int, int) range, char mode) {
             int pos = 0;
             int min = 0;
             int max = 0;
-            //iterate between minX and maxX
+            //iterate between min and max
             //for each:
-            //if x is starting velocity, step through and if there's ever a point where min < x < max, break
+            //if n is starting velocity, step through and if there's ever a point where min < n < max, break
             List<bool> tested = new List<bool>();
-            for(int i = minX; i <= maxX; i++) {
-                int start_x = i;
+            for(int i = min_val; i <= max_val; i++) {
+                int start = i;
                 while(true) {
-                    pos = step(pos, ref start_x,'x');
-                    if(pos >= xrange.Item1 && pos <= xrange.Item2) {
+                    step(ref pos, ref start, mode);
+                    if(pos >= range.Item1 && pos <= range.Item2) {
                         tested.Add(true);
                         break;
                     }
-                    if(start_x <= 0) {
+                    if(mode == 'x' && start <= 0) { //different exit conditions for x and y axis
+                        tested.Add(false);
+                        break;
+                    }
+                    if(mode == 'y' && pos < range.Item1) {
                         tested.Add(false);
                         break;
                     }
@@ -89,48 +92,7 @@ namespace Day17 {
                     break;
                 }
             }
-            return (minX+min,minX+max);
-        }
-
-        static (int,int) find_y(int minY, int maxY, (int, int) range) {
-            int init = -60;
-            int start_y = init;
-            int pos = 0;
-            int min = 1;
-            int max = 1;
-            //iterate between n and 100
-            //for each:
-            //if starting velocity = y, step through and if there's ever a point where min < y < max, set min and break
-            List<(bool,int)> tested = new List<(bool,int)>();
-            for(int i = minY; i <= maxY; i++) {
-                start_y = i;
-                while(true) {
-                    pos = step(pos, ref start_y,'y');
-                    if(pos >= range.Item1 && pos <= range.Item2) {
-                        min = init;
-                        tested.Add((true,i));
-                        break;
-                    }
-                    if(pos < range.Item1){
-                        tested.Add((false,i));
-                        break;
-                    }
-                }
-                pos = 0;
-            }
-            for(int i = 0; i < tested.Count; i++) {
-                if(tested[i].Item1 == true) {
-                    min = i;
-                    break;
-                }
-            }
-            for(int i = tested.Count - 1; i >= 0; i--) {
-                if(tested[i].Item1 == true) {
-                    max = i;
-                    break;
-                }
-            }
-            return (minY + min, minY + max);
+            return (min_val + min, min_val + max); //inc low value by min/max range to get total range of options
         }
         static int find_height(int x, int y) {
             int xpos = 0;
@@ -162,14 +124,13 @@ namespace Day17 {
                     return false;
             }
         }
-        static int step(int pos, ref int vel, char dir) {
+        static void step(ref int pos, ref int vel, char dir) {
             pos += vel;
             if(dir == 'x' && vel <= 0) {
-                return pos;
+                return;
             }
             else
                 vel -= 1;
-            return pos;
         }
         static void full_step(ref int xpos, ref int ypos, ref int xvel, ref int yvel) {
             xpos += xvel;
