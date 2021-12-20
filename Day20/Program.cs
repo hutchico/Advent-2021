4921bff4863d42994a86ba5ae2c7c1c5aa396642
hutchico <22420744+hutchico@users.noreply.github.com>
2021-12-20 11:59:33 -0800

    optimization pass

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Day20 {
@@ -12,7 +12,7 @@ namespace Day20 {
             using(StreamReader file = new StreamReader("input.txt")) {
                 IEA = file.ReadLine().ToList();
                 file.ReadLine(); //discard empty line
-                while(!file.EndOfStream){
+                while(!file.EndOfStream) {
                     image_raw.Add(file.ReadLine().ToList());
                 }
             }
@@ -25,45 +25,43 @@ namespace Day20 {
             }
 
             //part 1: i < 2
-            //part 2: i < 50
-            for(int i = 0; i < 50; i++){
+            for(int i = 0; i < 2; i++) {
                 image = apply_iea(image, IEA, i % 2);
             }
-            Console.WriteLine("Number of lit pixels counted: " + count(image));
+            Console.WriteLine("2 passes number of lit pixels counted: " + count(image));
+            //part 2: i < 50
+            for(int i = 2; i < 50; i++) {
+                image = apply_iea(image, IEA, i % 2);
+            }
+            Console.WriteLine("50 passes number of lit pixels counted: " + count(image));
         }
 
         static char[,] apply_iea(char[,] image, List<char> iea, int mode) {
             int sizeX, sizeY;
             List<char> chars;
-            char[,] newimg;
+            char[,] copy;
             char[,] paste;
             sizeX = image.GetLength(1) + 4; //expand boundaries to account for "infinity"
             sizeY = image.GetLength(0) + 4;
-            newimg = new char[sizeY, sizeX];
+            copy = new char[sizeY, sizeX];
             paste = new char[sizeY - 2, sizeX - 2];
-            for(int i = 0; i < paste.GetLength(1); i++) {
-                for(int j = 0; j < paste.GetLength(0); j++) {
-                    paste[j, i] = '.';
-                }
-            }
-            for(int i = 0; i < newimg.GetLength(1); i++) {
-                for(int j = 0; j < newimg.GetLength(0); j++) {
-                    if(i <= 1 || j <= 1 || i >= newimg.GetLength(1) - 2 || j >= newimg.GetLength(0) - 2)
-                        newimg[j, i] = mode == 1 ? '#' : '.'; //eric you slimy little—
+            for(int i = 0; i < copy.GetLength(1); i++) {
+                for(int j = 0; j < copy.GetLength(0); j++) {
+                    if(i <= 1 || j <= 1 || i >= copy.GetLength(1) - 2 || j >= copy.GetLength(0) - 2)
+                        copy[j, i] = mode == 1 ? '#' : '.'; //eric you cheeky little—
                     else
-                        newimg[j, i] = image[j - 2, i - 2];
+                        copy[j, i] = image[j - 2, i - 2];
                 }
             }
-           
-            for(int i = 1; i < newimg.GetLength(1) - 1; i++) {
-                for(int j = 1; j < newimg.GetLength(0) - 1; j++) {
+            for(int i = 1; i < copy.GetLength(1) - 1; i++) {
+                for(int j = 1; j < copy.GetLength(0) - 1; j++) {
                     chars = new List<char>();
-                    for(int k = -1; k < 2; k++){
+                    for(int k = -1; k < 2; k++) {
                         for(int l = -1; l < 2; l++) {
-                            chars.Add(newimg[j + k, i + l]);
+                            chars.Add(copy[j + k, i + l]);
                         }
                     }
-                    paste[j-1, i-1] = iea[compute_binary(chars)];
+                    paste[j - 1, i - 1] = iea[compute_binary(chars)];
                 }
             }
             return paste;
@@ -75,15 +73,6 @@ namespace Day20 {
                 bin += chars[i] == '#' ? '1' : '0';
             return Convert.ToInt32(bin, 2);
         }
-
-        static void print_image(char[,] image) {
-            for(int i = 0; i < image.GetLength(0); i++) {
-                for(int j = 0; j < image.GetLength(1); j++) {
-                    Console.Write(image[i, j]);
-                }
-                Console.Write('\n');
-            }
-        }
         static int count(char[,] image) {
             int num = 0;
             for(int i = 0; i < image.GetLength(0); i++) {
@@ -92,6 +81,14 @@ namespace Day20 {
                 }
             }
             return num;
+        }
+        static void print_image(char[,] image) {
+            for(int i = 0; i < image.GetLength(0); i++) {
+                for(int j = 0; j < image.GetLength(1); j++) {
+                    Console.Write(image[i, j]);
+                }
+                Console.Write('\n');
+            }
         }
     }
 }
