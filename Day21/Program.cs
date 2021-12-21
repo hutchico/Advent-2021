8a28e7a9d794a26187a3dd110307d4de203d3438
hutchico <22420744+hutchico@users.noreply.github.com>
2021-12-21 00:06:31 -0800

    day 21 partial commit

---
+++ Day21/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day21 {
+    class Program {
+        static void Main() {
+            int p1, p2;
+            Board board;
+            using(StreamReader file = new StreamReader("input.txt")) {
+                string[] tk = file.ReadLine().Split(':');
+                tk[1] = tk[1].Remove(0, 1);
+                p1 = Convert.ToInt32(tk[1], 10);
+                tk = file.ReadLine().Split(':');
+                tk[1] = tk[1].Remove(0, 1);
+                p2 = Convert.ToInt32(tk[1], 10);
+            }
+
+            board = new Board(p1, p2);
+            bool flip = false;
+            while(flip == false) {
+                flip = board.turn('1');
+                if(flip)
+                    break;
+                flip = board.turn('2');
+            }
+
+            
+        }
+    }
+
+    class Board {
+        public Board(int p1, int p2) {
+            P1pos = p1;
+            P2pos = p2;
+            die = 0; //initialize before first roll
+            p1score = 0;
+            p2score = 0;
+            dicerolls = 0;
+        }
+
+        public bool turn(char player) {
+            int rolled = 0;
+            for(int i = 0; i < 3; i++)
+                rolled += roll();
+            if(player == '1') {
+                P1pos += rolled;
+                while(P1pos > 10)
+                    P1pos -= 10;
+                p1score += P1pos;
+                return check_win(player);
+            }
+            else {
+                P2pos += rolled;
+                while(P2pos > 10)
+                    P2pos -= 10;
+                p2score += P2pos;
+                return check_win(player);
+            }
+        }
+
+        public bool check_win(char player) {
+            if(player == '1' && p1score >= 1000) {
+                Console.WriteLine("Win for player 1. final score: " + (p2score * dicerolls));
+                return true;
+            }
+            else if(player == '2' && p2score >= 1000) {
+                Console.WriteLine("Win for player 2. final score: " + (p1score * dicerolls));
+                return true;
+            }
+            return false;
+        }
+
+        private int roll() {
+            die++;
+            if(die > 100)
+                die = 1;
+            dicerolls++;
+            return die;
+        }
+
+        private int dicerolls;
+        private int p1score;
+        private int p2score;
+        private int die;
+        private int P1pos;
+        private int P2pos;
+    }
+}
